17486a31b1c9abff6f9300612f776e76b7ea4875
Alexandar Castaneda <alexthedar@gmail.com>
2025-09-17 09:54:06 -0700

    Nav

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,22 @@ import React from "react";
 import { useTheme } from "./providers/theme/ThemeContext";
 import ProposalView from "./views/ProposalView";
 import Navbar from "./components/Navbar";
+import WalletView from "./views/WalletView";
 // import WalletView from "./views/WalletView";
+
+const Pages = () => {
+  const currentPage: string = "/";
+
+  switch (currentPage) {
+    case "/":
+      return <ProposalView />;
+    case "/wallet":
+      return <WalletView />;
+
+    default:
+      return <div className="text-center">Page not found</div>;
+  }
+};
 
 const App: React.FC = () => {
   const { darkMode } = useTheme();
@@ -12,8 +27,7 @@ const App: React.FC = () => {
       <div className="min-h-screen bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
         <Navbar />
         <div className="max-w-screen-xl m-auto pt-16">
-          <ProposalView />
-          {/* <WalletView /> */}
+          <Pages />
         </div>
       </div>
     </div>
