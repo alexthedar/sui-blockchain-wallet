f1bb9ac19350ff74c2a59f8da7d97dcc4dc484dd
Alexandar Castaneda <alexthedar@gmail.com>
2025-09-17 11:14:40 -0700

    coin balance

--- frontend/src/components/wallet/SuiObject.tsx
+++ frontend/src/components/wallet/SuiObject.tsx
@@ -7,6 +7,12 @@ type SuiObjProps = {
 
 export const SuiObject: FC<SuiObjProps> = ({ objRes }) => {
   const owner = objRes.data?.owner;
+  const objectType = objRes.data?.type;
+
+  const isCoin = objectType?.includes("0x2::coin::Coin");
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const balance = isCoin ? (objRes.data?.content as any).fields?.balance : -1;
   return (
     <div className="p-2 border rounded-lg bg-gray-50 dark:text-gray-800">
       <p className="text-gray-700 dark:text-gray-300">
@@ -21,6 +27,11 @@ export const SuiObject: FC<SuiObjProps> = ({ objRes }) => {
           ? owner.AddressOwner
           : "Unknown Owner"}
       </p>
+      {isCoin && (
+        <p className="text-gray-700 dark:text-gray-300">
+          <strong>Balance:</strong> {balance} Mist
+        </p>
+      )}
     </div>
   );
 };
