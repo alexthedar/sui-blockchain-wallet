90d9cbc08d7d9706d457abd1dca79c3cb38fbf80
Alexandar Castaneda <alexthedar@gmail.com>
2025-09-17 10:08:41 -0700

    janky Nav working 2

--- frontend/src/providers/theme/navigation/NavigationProvider.tsx
+++ frontend/src/providers/theme/navigation/NavigationProvider.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NavigationContext } from "./NavigationContext";
 
 type NavigationProps = {
@@ -6,9 +6,21 @@ type NavigationProps = {
 };
 
 const NavigationProvider: React.FC<NavigationProps> = ({ children }) => {
-  const [currentPage, setCurrentPage] = useState("/");
+  const [currentPage, setCurrentPage] = useState(window.location.pathname);
 
-  const navigate = (page: string) => setCurrentPage(page);
+  const navigate = (page: string) => {
+    setCurrentPage(page);
+    window.history.pushState({}, "", page);
+  };
+
+  useEffect(() => {
+    const handlePopState = () => {
+      setCurrentPage(window.location.pathname);
+    };
+
+    window.addEventListener("popstate", handlePopState);
+    return () => window.removeEventListener("popstate", handlePopState);
+  }, []);
 
   return (
     <NavigationContext.Provider value={{ currentPage, navigate }}>
