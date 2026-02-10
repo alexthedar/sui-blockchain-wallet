0adf8be738a98d3e4d33bc6334113def924db7eb
Alexandar Castaneda <alexthedar@gmail.com>
2026-02-10 14:00:02 -0800

    modal

--- frontend/src/components/proposal/ProposalItem.tsx
+++ frontend/src/components/proposal/ProposalItem.tsx
@@ -1,13 +1,15 @@
 import { useSuiClientQuery } from "@mysten/dapp-kit";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { EcText } from "../Shared";
 import { SuiObjectData } from "@mysten/sui/client";
+import { VoteModal } from "./VoteModal";
 
 type ProposalItemProps = {
   id: string;
 };
 
 export const ProposalItem: FC<ProposalItemProps> = ({ id }) => {
+  const [isModelOpen, setModalOpen] = useState(false);
   const {
     data: dataResponse,
     isPending,
@@ -28,10 +30,33 @@ export const ProposalItem: FC<ProposalItemProps> = ({ id }) => {
   if (!proposal) return <EcText text="No data found" />;
 
   return (
-    <div className="p-4 border rounded-lg shadow-sm bg-white dark:bg-slate-800 hover:border-blue-500 transition-colors">
-      <h2 className="text-xl font-semibold mb-2">Title: {proposal.title}</h2>
-      <p className="text-gray-700 dark:text-gray-300">{proposal.description}</p>
-    </div>
+    <>
+      <div
+        onClick={() => setModalOpen(true)}
+        className="p-4 border rounded-lg shadow-sm bg-white dark:bg-slate-800 hover:border-blue-500 transition-colors"
+      >
+        <h2 className="text-xl font-semibold mb-2">Title: {proposal.title}</h2>
+        <p className="text-gray-700 dark:text-gray-300">
+          {proposal.description}
+        </p>
+        <div className="flex items-center justify-between mt-4">
+          <div className="flex space-x-4">
+            <div className="flex items-center text-green-600">
+              <span className="mr-1">👍</span>
+              {proposal.votedYesCount}
+            </div>
+            <div className="flex items-center text-red-600">
+              <span className="mr-1">👎</span>
+              {proposal.votedNoCount}
+            </div>
+          </div>
+          <div>
+            <EcText text={formatUnixTime(proposal.expiration)} />
+          </div>
+        </div>
+      </div>
+      <VoteModal isOpen={isModelOpen} onClose={() => setModalOpen(false)} />
+    </>
   );
 };
 
@@ -48,4 +73,15 @@ function parseProposal(data: SuiObjectData) {
     votedNoCount: Number(voted_no_count),
     expiration: Number(expiration),
   };
+}
+
+function formatUnixTime(timestampSec: number) {
+  return new Date(timestampSec * 1000).toLocaleString("en-US", {
+    month: "short",
+    day: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
 }
